3496ae53b9022464dea2e4ab1f20b574f02b0a8a
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2024-04-01 00:08:27 -0300

    feat: create appointment service

--- app/Services/CreateAppointmentService.php
+++ app/Services/CreateAppointmentService.php
@@ -2,10 +2,39 @@
 
 namespace App\Services;
 
+use Google\Client;
+use Google\Service\Calendar;
+use Google\Service\Calendar\Event;
+use Illuminate\Support\Carbon;
+
 class CreateAppointmentService
 {
-    public function run(): void
+    private readonly Calendar $calendarService;
+
+    public function __construct()
     {
-        logger('Horário marcado com sucesso!');
+        $client = new Client();
+        $client->setAuthConfig(__DIR__ . '../../google-calendar-key.json');
+        $client->setRedirectUri('http://18.224.69.123');
+        $client->addScope(Calendar::CALENDAR);
+
+        $this->calendarService = new Calendar($client);
+    }
+
+    public function run(string $summary, Carbon $start, Carbon $end): void
+    {
+        $data = new Event([
+            'summary' => $summary,
+            'start' => [
+                'dateTime' => $start->toIso8601String(),
+                'timeZone' => 'America/Fortaleza',
+            ],
+            'end' => [
+                'dateTime' => $end->toIso8601String(),
+                'timeZone' => 'America/Fortaleza',
+            ],
+        ]);
+
+        $this->calendarService->events->insert('ermeson.sampaio.queiroz@gmail.com', $data);
     }
 }
