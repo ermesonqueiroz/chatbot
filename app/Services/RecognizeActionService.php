191cf31a084afcc3584404198cc7a42f1b16872c
Ermeson Sampaio <ermeson.s.queiroz@gmail.com>
2024-04-01 22:53:41 -0300

    fix: appointment date in iso 8601

--- app/Services/RecognizeActionService.php
+++ app/Services/RecognizeActionService.php
@@ -19,7 +19,7 @@ class RecognizeActionService
         Quando o cliente pedir para marcar um horário e informar o dia e a hora do agendamento retorne uma mensagem no seguinte formato: create_appointment,<data_do_agendamento>.
         Caso seja apenas uma mensagem pedindo uma informação ou algo que não gere um efeito colateral no back-end retorne uma mensagem no seguinte formato: no_action,null
 
-        A <data_do_agendamento> deve seguir o seguinte formato <DIA/MES/ANO> <HORA> (leve em consideração a data de hoje sendo $today->localeDayOfWeek - {$today->toDateTimeLocalString()})
+        A <data_do_agendamento> deve seguir o formato ISO 8601 (leve em consideração a data de hoje sendo {$today->toIso8601String()})
         EOD;
 
 
